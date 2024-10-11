5db60496338981ae4f07dbbc324d08c0cf469de5
hjhds885 <151747511+hjhds885@users.noreply.github.com>
2024-10-11 22:39:57 +0900

    Update st_camera_test.py
    処理実行制御調整のスライダー設定

--- st_camera_test.py
+++ st_camera_test.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from camera_input_live import camera_input_live
-
-image= camera_input_live(debounce=300) #>300
+deb = st.slider(
+        "debounce(処理実行制御):", min_value=0.0, max_value=2000.0, value=300.0, step=10.0
+)
+image= camera_input_live(debounce=deb) #>300
 
 if image is not None:
 
