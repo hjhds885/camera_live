eeddaa09913ceb832cb47959e631a1f8eca0c678
hjhds885 <151747511+hjhds885@users.noreply.github.com>
2024-10-11 22:52:10 +0900

    Update st_camera_test.py
    修正

--- st_camera_test.py
+++ st_camera_test.py
@@ -3,8 +3,10 @@ from camera_input_live import camera_input_live
 deb = st.slider(
         "debounce(処理実行制御):", min_value=0.0, max_value=2000.0, value=300.0, step=10.0
 )
+image = ""
 image= camera_input_live(debounce=deb) #>300
 
 if image is not None:
-
-    st.image(image)
+        st.image(image)
+        image=""
+        
